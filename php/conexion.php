53f5fd7538e6401c417c9e06d986cba34b8df7a5
MrSeba <seba.pincheirav@gmail.com>
2018-06-07 08:12:06 -0400

    conexion mod

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 //$conexion = mysqli_connect('127.0.0.1','root','', 'tienda');
 $conexion = mysqli_connect('127.0.0.1','root' ,'','contabilidad');
-mysql_select_db('contabilidad', $conexion);
+//mysql_select_db('contabilidad', $conexion);
 
 function fechaNormal($fecha){
 		$nfecha = date('d/m/Y',strtotime($fecha));
